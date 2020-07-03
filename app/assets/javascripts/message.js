bd20cc88ebbe5c32f583de3353d0f47ed9ddf554
Matsumaru-Kachimaru <64599456+Matsumaru-Kachimaru@users.noreply.github.com>
2020-07-03 21:11:17 +0900

    add auto update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,5 @@
 $(function(){ 
-  function buildHTML(message){
+  var buildHTML = function(message) {
     if ( message.image ) {
         var html =
          `<div class="message" data-message-id=${message.id}>
@@ -54,8 +54,8 @@ $(function(){
     })
     .done(function(data){
       var insertHTML = buildHTML(data);
-      $('.messages').append(insertHTML);
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      var msg='.message-list'
+      message(insertHTML,msg);
       $('form')[0].reset();
     })
     .always(function(){
@@ -65,4 +65,39 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     })
   });
+  
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    console.log(last_message_id)
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        var msg = '.messages';
+        $.each(messages, function(i,message) {
+          insertHTML += buildHTML(message)
+          console.log(message);
+        });
+        message(insertHTML,msg);
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
+
+
+function message(insertHTML,msg) {
+  $(msg).append(insertHTML);
+  $(msg).animate({ scrollTop: $(msg)[0]});
+};
