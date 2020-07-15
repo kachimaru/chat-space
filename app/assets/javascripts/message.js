6b09f5ac6b482567473f5ca7564c28ee3541244d
Matsumaru-Kachimaru <64599456+Matsumaru-Kachimaru@users.noreply.github.com>
2020-07-15 19:57:23 +0900

    add reload

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -65,4 +65,36 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     })
   });
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    console.log(last_message_id)
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        var msg = '.messages';
+        $.each(messages, function(i,message) {
+          insertHTML += buildHTML(message)
+          console.log(message);
+        });
+        message(insertHTML,msg);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+  setInterval(reloadMessages,7000);
 });
+
+function message(insertHTML,msg) {
+  $(msg).append(insertHTML);
+  $(msg).animate({ scrollTop: $(msg)[0]});
+};
