ed09d2233b0f9e23b9b2269c0f0edc7e773bbc56
Matsumaru-Kachimaru <64599456+Matsumaru-Kachimaru@users.noreply.github.com>
2020-07-23 12:15:03 +0900

    fixing

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'kachi-buncket'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/kachi-buncket'
+  config.fog_directory  = 'kachi-chatspace'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/kachi-chatspace'
 end
